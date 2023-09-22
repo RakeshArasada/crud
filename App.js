a6afd623ce89818769df51ff6b006650af6a98e8
RakeshArasada <rakesh.a@zizmu.in>
2023-09-22 15:36:06 +0530

    commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import DriverList from './Components/DriverList';
+
+function App() {
+  return (
+    <div className="App">
+      <DriverList/>
+    </div>
+  );
+}
+
+export default App;
